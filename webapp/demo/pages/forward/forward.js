5ba86c92e70ccb68b57ba919cc4ad136b7adb341
wanglei <wanglei@medlinker.com>
2016-05-30 14:55:36 +0800

    h5

--- webapp/demo/pages/forward/forward.js
+++ webapp/demo/pages/forward/forward.js
@@ -23,10 +23,30 @@
                     this.forward('index', null, null, 'pop');
                 },
                 'click .js-btn2': function () {
-//这里没写好,要改造
                     this.forward('index', null, null, 'present');
+                },
+                'click .js-btn3': function () {
+_.requestHybrid({
+    tagname: 'forward',
+    param: {
+        topage: 'https://www.baidu.com/',
+        type: 'h5'
+    }
+});
+                },
+                'click .js-btn4': function () {
+_.requestHybrid({
+    tagname: 'forward',
+    param: {
+        topage: 'index2',
+        type: 'native',
+        navigateion: 'none'
+    }
+});
                 }
-            }
+
+
+            };
 
         },
 
